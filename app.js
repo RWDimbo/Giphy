4e12bc225c903de97ad483e2d48650563ae52586
RDimbo <r.dimbo@outlook.com>
2019-06-20 21:55:26 -0400

    Fixed the errors

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 var subjects = ["Star Wars", "Street FIghter II", "Jonny Quest", "Elite Dangerous", "XCOM"];
 
 function displayGifs() {
-
+  ('displayGifs', this)
   var subjects = $(this).attr("data-name");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + subjects + "&api_key=YG4Y7s542RabyCVPvWtSZdvlIzYe5NTF&limit=10"
-console.log(subjects);
- 
+  (subjects);
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    $("#gif-appear-here").empty();
-    
+    $("#gifs-appear-here").empty();
     var results = response.data;
-    console.log(results);
+    (results);
     for (var i = 0; i < results.length; i++) {
       // Omit mature content
       if (results[i].rating !== "r" && results[i].rating !== "PG-13") {
@@ -23,7 +22,7 @@ console.log(subjects);
         var animated = results[i].images.fixed_height.url;
         var still = results[i].images.fixed_height_still.url
         var subjectImage = $("<img class=gifImage>");
-             
+
         //Starting with stills
         subjectImage.attr('src', still);
         subjectImage.attr('data-still', still)
@@ -35,30 +34,35 @@ console.log(subjects);
 
         $("#gifs-appear-here").prepend(gifDiv)
       }
-    
+
     }
   });
 
-    // Onclick function to animate/pause gifs
-$('#gifs-appear-here').on("click", ".gifImage", function() {
-//console.log("Image clicked");
+}
+// Onclick function to animate/pause gifs
+$(document).on("click", ".gifImage", function () {
+ ("Image clicked", this);
   var state = $(this).attr('data-state');
+  var animate = $(this).attr('data-animate')
+  var still = $(this).attr('data-still')
   // If state = still, on click will animate the gif
+ ("state", state)
+ ("still", still)
+ ("animate", animate)
   if (state === 'still') {
-      $(this).attr('src', $(this).attr('data-animate'));
-      $(this).attr('data-state', 'animate');}
+    $(this).attr('src', animate);
+    $(this).attr('data-state', 'animate');
+  }
   // Otherwise, if state != still, gif will pause on click   
   else {
-      $(this).attr('src', $(this).data('still'));
-      $(this).attr('data-state', 'still');
+    $(this).attr('src', still);
+    $(this).attr('data-state', 'still');
   }
 });
-}
 
-
-
-    //Creates Buttons
+//Creates Buttons
 function renderButtons() {
+ ('renderButtons', this)
   $('#buttons-view').empty();
   for (var i = 0; i < subjects.length; i++) {
     var addButton = $("<button>");
@@ -67,22 +71,20 @@ function renderButtons() {
     addButton.text(subjects[i]);
     $("#buttons-view").append(addButton);
   }
-  };
+};
 
-    // Single button clicked
-    $("#add-choice").on("click", function (event) {
-      event.preventDefault();
-      var choice = $("#input-choice").val().trim();
-      subjects.push(choice);
-      renderButtons();
-      $("#input-choice").val("");
-
-      
-  
-    })
+// Single button clicked
+$("#add-choice").on("click", function (event) {
+ ('addChoice', this)
+  event.preventDefault();
+  var choice = $("#input-choice").val().trim();
+  subjects.push(choice);
+ (subjects, choice)
+  renderButtons();
+  $("#input-choice").val("");
 
 
 
-
-$(document).on("click", ".subjects", displayGifs);
+})
 renderButtons();
+$(document).on("click", ".subjects", displayGifs);
